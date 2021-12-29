c85b70b97abead285d1268add784bc26b6c3c65d
Hgaffa <hgaffa@gmail.com>
2021-12-29 23:01:54 +0000

    Fix contact form email

--- BarberCrewV2/mail_handler.php
+++ BarberCrewV2/mail_handler.php
@@ -100,7 +100,7 @@ if($_POST) {
 
     $headers  = 'MIME-Version: 1.0' . "\r\n"
 
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'Content-type: text/html; charset=iso-8859-1' . "\r\n"
 
     .'From: ' . $visitor_email . "\r\n";
 
