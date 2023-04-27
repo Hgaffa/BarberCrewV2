37d2e7b71a05cfe83135326aedefafe028745bca
Hgaffa <hgaffa@gmail.com>
2023-04-27 11:31:09 +0100

    slider update

---
+++ BarberCrewV2/js/slick.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function($) {
+    $('.gallery-slider').slick({
+        infinite: true,
+        lazyLoad: 'ondemand',
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: false,
+        responsive: [{
+            breakpoint: 800,
+            settings: {
+                arrows: false,
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 500,
+            settings: {
+                arrows: false,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+    $('.team-slider').slick({
+        infinite: true,
+        speed: 500,
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: true,
+        responsive: [{
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+});
