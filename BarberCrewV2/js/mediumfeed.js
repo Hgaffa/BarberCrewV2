5a630bd8a981af90d166831fa8e4d5e1d810bc9a
Hgaffa <hgaffa@gmail.com>
2023-04-28 15:53:57 +0100

    blog section + slick slider updates

--- BarberCrewV2/js/mediumfeed.js
+++ BarberCrewV2/js/mediumfeed.js
@@ -1,5 +1,6 @@
-const ul = document.getElementById('blogs');
+const ul = document.getElementById('blog-feed');
 const url = 'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@barbercrew';
+const mediumProfile = 'https://medium.com/@barbercrew';
 
 fetch(url)
     .then((res) => res.json())
@@ -15,18 +16,47 @@ fetch(url)
             return node;
         }
 
+        function BlogSliderSetting() {
+            return {
+                infinite: true,
+                speed: 500,
+                lazyLoad: 'ondemand',
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                arrows: true,
+                dots: true,
+                responsive: [{
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                }, {
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }]
+            }
+        }
+
         let output = '';
+        $('.blog-slider').slick(BlogSliderSetting());
         posts.slice(0, 3).forEach((item) => {
-            output += `
-         <li class="post">
-               <a href="${item.link}"><img src="${item.thumbnail}" class="topImg"></img>
-               <div class="content">
-                  <div class="preview">
-                     <h2 class="blog_title">${item.title}</h2></a>
-										 <button class="moreBtn" type="button" onclick="location.href='${item.link}'">Read More</button>
-                  </div>                
-               </div>
-         </li>`
+            output = `
+            <div class="blog-card">
+                <img class="img-container" width=200px; src="${item.thumbnail}"></img>
+                <div class="blog-card-content">
+                    <a href="${item.link}"><h1>${item.title}</h1></a>
+                    <p class="excerpt">${item.content.slice(0,155)}...</p>
+                    <a href="${mediumProfile}"><p class="author">${item.pubDate.slice(0,10)}</p></a>
+                </div>
+            </div>
+            `
+            $('.blog-slider').slick('slickAdd', output);
         });
-        ul.innerHTML = output;
+
     });
